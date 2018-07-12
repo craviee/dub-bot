b06945416485c339fb7350bb8e386dc10a90ee40
Craviee <danielcraviee@gmail.com>
2018-07-12 11:34:24 -0300

    correcting server.js

--- server.js
+++ server.js
@@ -1,108 +1,290 @@
-//  OpenShift sample Node application
-var express = require('express'),
-    app     = express(),
-    morgan  = require('morgan');
-    
-Object.assign=require('object-assign')
+var DubAPI = require('dubapi');
 
-app.engine('html', require('ejs').renderFile);
-app.use(morgan('combined'))
+new DubAPI({username: 'Putinha-Bot', password: '123456789'}, function(err, bot) {
+    if (err) return console.error(err);
 
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
-    mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
-    mongoURLLabel = "";
+    var resp=0;
+    var count_fila=0;
+    var count_downs = { "downdubs":"0"};
+    //var teste = 0;
+    var chamada = 0;
+    var fila = [];
+    var fila_user = [];
+    var presentes = [];
+    var retirados = 0;
+    var msg;
 
-if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
-  var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
-      mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
-      mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
-      mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
-      mongoPassword = process.env[mongoServiceName + '_PASSWORD']
-      mongoUser = process.env[mongoServiceName + '_USER'];
-
-  if (mongoHost && mongoPort && mongoDatabase) {
-    mongoURLLabel = mongoURL = 'mongodb://';
-    if (mongoUser && mongoPassword) {
-      mongoURL += mongoUser + ':' + mongoPassword + '@';
-    }
-    // Provide UI label that excludes user id and pw
-    mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
-    mongoURL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
-
-  }
-}
-var db = null,
-    dbDetails = new Object();
-
-var initDb = function(callback) {
-  if (mongoURL == null) return;
-
-  var mongodb = require('mongodb');
-  if (mongodb == null) return;
-
-  mongodb.connect(mongoURL, function(err, conn) {
-    if (err) {
-      callback(err);
-      return;
-    }
-
-    db = conn;
-    dbDetails.databaseName = db.databaseName;
-    dbDetails.url = mongoURLLabel;
-    dbDetails.type = 'MongoDB';
-
-    console.log('Connected to MongoDB at: %s', mongoURL);
-  });
+    bla = function(){
+    return new Promise(function(fulfill, reject){
+        count_downs = bot.getScore();
+        fulfill(resultValue);
+    });
 };
 
-app.get('/', function (req, res) {
-  // try to initialize the db on every request if it's not already
-  // initialized.
-  if (!db) {
-    initDb(function(err){});
-  }
-  if (db) {
-    var col = db.collection('counts');
-    // Create a document with request IP and current time of request
-    col.insert({ip: req.ip, date: Date.now()});
-    col.count(function(err, count){
-      if (err) {
-        console.log('Error running count. Message:\n'+err);
-      }
-      res.render('index.html', { pageCountMessage : count, dbInfo: dbDetails });
+    console.log('Running DubAPI v' + bot.version);
+
+    function connect() {bot.connect('radioanao');}
+
+    function skip ()
+    {
+        count_downs = bot.getScore();
+    }
+
+    skip = function(){
+
+    return new Promise(function(fulfill, reject){
+        count_downs = bot.getScore();
+        fulfill(count_downs);
     });
-  } else {
-    res.render('index.html', { pageCountMessage : null});
-  }
-});
+};
 
-app.get('/pagecount', function (req, res) {
-  // try to initialize the db on every request if it's not already
-  // initialized.
-  if (!db) {
-    initDb(function(err){});
-  }
-  if (db) {
-    db.collection('counts').count(function(err, count ){
-      res.send('{ pageCount: ' + count + '}');
+
+
+    function skip2()
+    {
+        count_downs = count_downs.downdubs;
+            console.log(count_downs);
+    }
+
+
+    bot.on('connected', function(name) {
+        console.log('Connected to ' + name);
+
+        // while(1)
+        // {
+        //     count_downs = bot.getScore();
+        //     while(count_downs == null);
+        //     count_downs = count_downs.downdubs;
+        //     console.log(count_downs);
+        //     //myPromise.then(console.log(count_downs));
+        //     //count_downs// = resp.downdubs;
+        //     //console.log("count_downs");
+        //     if(count_downs !== 0 && count_downs != null)
+        //     {
+        //         resp = bot.getQueue();
+        //         count_fila = resp.length;
+        //         console.log(count_downs + " " + count_fila);
+        //         if(count_fila <= 2 && count_downs >=3)
+        //             bot.moderateSkip();
+        //         else if (count_downs >= count_fila/2)
+        //             bot.moderateSkip();
+        //     }
+        //     //wait(5000);
+        // }
+
+
     });
-  } else {
-    res.send('{ pageCount: -1 }');
+
+    function wait(ms){
+   var start = new Date().getTime();
+   var end = start;
+   while(end < start + ms) {
+     end = new Date().getTime();
   }
+}
+
+    bot.on('disconnected', function(name) {
+        console.log('Disconnected from ' + name);
+
+        setTimeout(connect, 15000);
+    });
+
+    bot.on('error', function(err) {
+        console.error(err);
+    });
+
+    bot.on(bot.events.userJoin, function(name)
+    {
+        console.log(name);
+        //bot.sendChat(resp.length + " pessoas na fila :3");
+    });
+
+    bot.on(bot.events.roomPlaylistDub, function(data)
+    {
+        if(data.type == "room_playlist-dub" && data.dubtype == "downdub")
+        {
+            msg = data;
+            count_downs = msg.raw.playlist.downdubs;
+            //console.log(data.playlists);
+            //bot.sendChat("Trigger por downvote");
+            //song = new PlayModel(msg.playlist);
+            resp = bot.getQueue();
+            //count_downs = data.playlist;
+            count_fila = resp.length;
+            //console.log(count_downs + " " + count_fila);
+            if(count_fila <= 2 && count_downs >=3)
+            {
+                bot.moderateSkip();
+                bot.sendChat( count_fila + " na fila, " + count_downs + " downvotes.");
+            }
+            else if ((count_downs >= count_fila/2) && count_fila > 2)
+            {
+                bot.moderateSkip();
+                bot.sendChat( count_fila + " na fila, " + count_downs + " downvotes.");
+            }
+        }
+
+    });
+
+
+    bot.on(bot.events.chatMessage, function(data) {
+        if((data.message.indexOf("help") !== -1) && (data.user.username.indexOf("Putinha-Bot") == -1) && (data.message.indexOf("@Putinha-Bot") !== -1) && (bot.isMod(data.user) || bot.isManager(data.user) || bot.isOwner(data.user) ||  bot.isCreator(data.user)))
+        {
+             bot.sendChat("Para a `Staff` realizar chamada, basta me quotar com a palavra `chamada` e após a contagem, me quotar com a palavra `finalizar`.");
+        }
+        // else if((data.message.indexOf("help") !== -1) && (data.user.username.indexOf("Putinha-Bot") == -1) && (data.message.indexOf("@Putinha-Bot") !== -1))
+        // {
+        //     bot.sendChat("Caso a música tenha os `downvotes necessários`, basta digitar algo no chat para que eu `pule` a música.")
+        // }
+        else if(data.message.indexOf("@Putinha-Bot") !== -1)
+        {
+            if(bot.isMod(data.user) || bot.isManager(data.user) || bot.isOwner(data.user) ||  bot.isCreator(data.user) || (data.user.username.indexOf("Anao_Engenheiro") !== -1))
+            {
+               //bot.sendChat("@" + data.user.username + " é da Staff.");
+               if(data.message.indexOf("chamada") !== -1 && chamada == 0)
+               {
+                    if(presentes.indexOf(data.user.username) == -1)
+                    {
+                        presentes.push(data.user.username);
+                    }
+                    resp = bot.getQueue();
+                    console.log(resp);
+                    //resp = JSON.parse(resp);
+                    for(var i = 0; i < resp.length; i++)
+                    {
+                        //console.log(resp[i].user.username);
+                        //bot.sendChat(resp[i].user.username);
+                        fila.push(resp[i].user.username);
+                        fila_user.push(resp[i].uid);
+                        console.log("fila[" + i + "] = " + fila[i]);
+                    }
+
+                    chamada = 1;
+
+                    for(var i =0; i < 5; i++)
+                        bot.sendChat("@djs CHAMADA " + (i+1) + "/5");// CHAMADA RESPONDAM APENAS UMA VEZ " + (i+1) + "/5");
+               }
+               else if((data.message.indexOf("finalizar") !== -1) && (chamada == 1))
+               {
+                    if(presentes.indexOf(data.user.username) == -1)
+                    {
+                        presentes.push(data.user.username);
+                    }
+                //count_downs = bot.getScore();
+                    chamada = 0;
+                    for(var i = 0; i < fila.length; i++)
+                        console.log("fila: " + fila[i]);
+                    for(var i = 0; i < presentes.length; i++)
+                        console.log("presentes: " + presentes[i]);
+                    //{
+                    for(var j = 0; j < fila.length; j++)
+                        if(presentes.indexOf(fila[j]) == -1)
+                        {
+                            //bot.moderateRemoveDJ(fila_user[j]);
+                            bot.sendChat("@mods `retirem o " + fila[j] + "`");
+                            retirados ++;
+                        }
+                    if(retirados == 0)
+                        bot.sendChat("`Todos estão presentes.`");
+                    else
+                        retirados = 0;
+                    fila = [];
+                    fila_user = [];
+                    presentes = [];
+                        //console.log("fila: " + fila[i]);
+                    //}
+
+                        //console.log("presentes: " + presentes[i]);
+                }
+                else if ((data.user.username.indexOf("Anao_Engenheiro") !== -1) && (data.message.indexOf("skip") !== -1))
+                    bot.moderateSkip();
+                else if (data.user.username.indexOf("Anao_Engenheiro") !== -1)
+                    bot.sendChat(":thumbsup:");
+
+               // else
+               //  bot.sendChat(":thumbsup: (me quote com `help` para mais informações) ");
+            // else if(data.message.indexOf("qual o sentido da vida?") !== -1)
+            //     bot.sendChat("Leitar e ser leitado.");
+            // else if(data.message.indexOf("ping") !== -1)
+            //     bot.sendChat("@" + data.user.username + " pong :3");
+            // //else if (data.user.username.indexOf("Poneiamigo") !== -1)
+            // //{
+            //     //bot.sendChat("@" + data.user.username + " sua puta.");
+            // //}
+            // else if ((data.message.indexOf("puta") !== -1) || (data.message.indexOf("vagabunda") !== -1))
+            //     bot.sendChat("Me enche de leitinho quente :3 , " + "@" + data.user.username);
+
+            }
+
+            // else
+            //     bot.sendChat(":thumbsup: (me quote com `help` para mais informações) ");
+        }
+            else if(chamada == 1)
+         {
+             //console.log(data.user.username + ': ' + data.message);
+            //  for(var i = 0; i < resp.length; i++)
+            // {
+            //     console.log( "fila[" + i + "] = " + fila[i] + " " + data.user.username);
+            //      if(fila[i] == data.user.username)
+            //      {
+            //          //bot.sendChat("@" + data.user.username + " presente");
+            //          console.log( "fila[" + i + "] = " + fila[i] + " presente");
+            //          fila[i] = 0;
+            //          //console.log( fila[fila.indexOf(data.user.username)] + " presente");
+            //          console.log( "fila[" + i + "] = " + fila[i] + " zerado");
+            //          break;
+            //      }
+            // }
+            if(presentes.indexOf(data.user.username) == -1)
+            {
+                presentes.push(data.user.username);
+            }
+         }
+
+
+        /*count_downs = bot.getScore();
+        count_downs = count_downs.downdubs;
+        if(count_downs !== 0 && count_downs != null && ((data.user.username.indexOf("Putinha-Bot") == -1)))
+        {
+            resp = bot.getQueue();
+            count_fila = resp.length;
+            //console.log(count_downs + " " + count_fila);
+            if(count_fila <= 2 && count_downs >=3)
+            {
+                bot.moderateSkip();
+                bot.sendChat( count_fila + " na fila, " + count_downs + " downvotes.");
+            }
+            else if ((count_downs >= count_fila/2) && count_fila > 2)
+            {
+                bot.moderateSkip();
+                bot.sendChat( count_fila + " na fila, " + count_downs + " downvotes.");
+            }
+        }*/
+
+        //console.log(data.user.username + ': ' + data.message);
+    });
+
+    connect();
+
+
+
+    // while(1)
+    // {
+    //     resp = bot.getScore();
+    //     console.log(resp);
+    //     //myPromise.then(console.log(count_downs));
+    //     count_downs = resp.downdubs;
+    //     console.log(count_downs);
+    //     if(count_downs !== 0)
+    //     {
+    //         resp = bot.getQueue();
+    //         count_fila = resp.length;
+    //         //console.log(count_downs + " " + count_fila);
+    //         if(count_fila <= 2 && count_downs >=3)
+    //             bot.moderateSkip();
+    //         else if (count_downs >= count_fila/2)
+    //             bot.moderateSkip();
+    //     }
+    // }
+
 });
-
-// error handling
-app.use(function(err, req, res, next){
-  console.error(err.stack);
-  res.status(500).send('Something bad happened!');
-});
-
-initDb(function(err){
-  console.log('Error connecting to Mongo. Message:\n'+err);
-});
-
-app.listen(port, ip);
-console.log('Server running on http://%s:%s', ip, port);
-
-module.exports = app ;
