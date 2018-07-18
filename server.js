f1bf0760e413a6c396c29c8a64f19a453eaba989
craviee <danielcraviee@gmail.com>
2018-07-17 22:24:19 -0300

    Bot can now remove DJs from list

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var DubAPI = require('dubapi');
 
-new DubAPI({username: 'Putinha-Bot', password: '123456789'}, function(err, bot) {
+new DubAPI({username: 'AnaoTestBot', password: '123456789'}, function(err, bot) {
     if (err) return console.error(err);
 
     var resp=0;
@@ -49,7 +49,17 @@ new DubAPI({username: 'Putinha-Bot', password: '123456789'}, function(err, bot) 
 
     bot.on('connected', function(name) {
         console.log('Connected to ' + name);
-
+        console.log("before");
+        // console.log(bot.getUserByName("Anao_Engenheiro"));
+        // resp = bot.getQueue();
+        // console.log(bot.getRoomMeta());
+        // console.log(resp);
+        // bot.sendChat("Vou agora retirar o Nyan");
+        //https://api.dubtrack.fm/room/57fd4c0f593d672e002a19e8/queue/user/587a90e5a57dd766009509f9/pause
+        // bot.moderatePauseDJ(bot.getUserByName("Anao_Engenheiro").id, function() {});
+        resp = bot.getQueue();
+        console.log(resp);
+        console.log("after");
         // while(1)
         // {
         //     count_downs = bot.getScore();
@@ -128,15 +138,15 @@ new DubAPI({username: 'Putinha-Bot', password: '123456789'}, function(err, bot) 
 
 
     bot.on(bot.events.chatMessage, function(data) {
-        if((data.message.indexOf("help") !== -1) && (data.user.username.indexOf("Putinha-Bot") == -1) && (data.message.indexOf("@Putinha-Bot") !== -1) && (bot.isMod(data.user) || bot.isManager(data.user) || bot.isOwner(data.user) ||  bot.isCreator(data.user)))
+        if((data.message.indexOf("help") !== -1) && (data.user.username.indexOf("AnaoTestBot") == -1) && (data.message.indexOf("@AnaoTestBot") !== -1) && (bot.isMod(data.user) || bot.isManager(data.user) || bot.isOwner(data.user) ||  bot.isCreator(data.user)))
         {
              bot.sendChat("Para a `Staff` realizar chamada, basta me quotar com a palavra `chamada` e após a contagem, me quotar com a palavra `finalizar`.");
         }
-        // else if((data.message.indexOf("help") !== -1) && (data.user.username.indexOf("Putinha-Bot") == -1) && (data.message.indexOf("@Putinha-Bot") !== -1))
+        // else if((data.message.indexOf("help") !== -1) && (data.user.username.indexOf("AnaoTestBot") == -1) && (data.message.indexOf("@AnaoTestBot") !== -1))
         // {
         //     bot.sendChat("Caso a música tenha os `downvotes necessários`, basta digitar algo no chat para que eu `pule` a música.")
         // }
-        else if(data.message.indexOf("@Putinha-Bot") !== -1)
+        else if(data.message.indexOf("@AnaoTestBot") !== -1)
         {
             if(bot.isMod(data.user) || bot.isManager(data.user) || bot.isOwner(data.user) ||  bot.isCreator(data.user) || (data.user.username.indexOf("Anao_Engenheiro") !== -1))
             {
@@ -180,8 +190,10 @@ new DubAPI({username: 'Putinha-Bot', password: '123456789'}, function(err, bot) 
                     for(var j = 0; j < fila.length; j++)
                         if(presentes.indexOf(fila[j]) == -1)
                         {
-                            //bot.moderateRemoveDJ(fila_user[j]);
-                            bot.sendChat("@mods `retirem o " + fila[j] + "`");
+                            bot.moderatePauseDJ(fila_user[j], function() {});
+                            //bot.moderateRemoveSong(fila_user[j]);
+                            console.log(fila_user[j]);
+                            bot.sendChat("Retirando @" + fila[j] + " da fila.");
                             retirados ++;
                         }
                     if(retirados == 0)
@@ -199,7 +211,24 @@ new DubAPI({username: 'Putinha-Bot', password: '123456789'}, function(err, bot) 
                 else if ((data.user.username.indexOf("Anao_Engenheiro") !== -1) && (data.message.indexOf("skip") !== -1))
                     bot.moderateSkip();
                 else if (data.user.username.indexOf("Anao_Engenheiro") !== -1)
-                    bot.sendChat(":thumbsup:");
+                {
+                    var testi = false;
+                    if(testi)
+                            console.log("fez coisa");
+                    console.log("entrou");
+                    bot.sendChat("Testando timer: 0", function () {
+                        testi = true;
+                        wait(1000*30);
+                        console.log("acabou espera");
+                        bot.sendChat("Apos 30 segundos");
+                    });
+                    // console.log("acabou espera");
+                    // bot.sendChat("Apos 30 segundos");
+
+                    // bot.moderatePauseDJ(bot.getUserByName("Anao_Engenheiro").id, function() {});
+                    // var resp = bot.getQueue();
+                    // console.log(resp);
+                }
 
                // else
                //  bot.sendChat(":thumbsup: (me quote com `help` para mais informações) ");
@@ -244,7 +273,7 @@ new DubAPI({username: 'Putinha-Bot', password: '123456789'}, function(err, bot) 
 
         /*count_downs = bot.getScore();
         count_downs = count_downs.downdubs;
-        if(count_downs !== 0 && count_downs != null && ((data.user.username.indexOf("Putinha-Bot") == -1)))
+        if(count_downs !== 0 && count_downs != null && ((data.user.username.indexOf("AnaoTestBot") == -1)))
         {
             resp = bot.getQueue();
             count_fila = resp.length;
