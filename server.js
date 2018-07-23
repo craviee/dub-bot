28346e45d1e1c1056c00d5e1108afa66c27e6b21
Craviee <danielcraviee@gmail.com>
2018-07-23 13:40:38 -0300

    several changes.

--- server.js
+++ server.js
@@ -1,319 +1,224 @@
-var DubAPI = require('dubapi');
+let DubAPI = require('dubapi');
 
-new DubAPI({username: 'AnaoTestBot', password: '123456789'}, function(err, bot) {
-    if (err) return console.error(err);
+new DubAPI({username: 'AnaoTestBot', password: '123456789'}, function(err, bot)
+{
+    function finishAttendanting(allAttendant)
+    {
+        // LOGS
+        // console.log("attendanting set to 0");
+        // for(var i = 0; i < queue.length; i++)
+        //     console.log("queue: " + queue[i]);
+        // for(var i = 0; i < attendingList.length; i++)
+        //     console.log("attendingList: " + attendingList[i]);
+        attendanting = 0;
 
-    var resp=0;
-    var count_fila=0;
-    var count_downs = { "downdubs":"0"};
-    //var teste = 0;
-    var chamada = 0;
-    var fila = [];
-    var fila_user = [];
-    var presentes = [];
-    var retirados = 0;
-    var msg;
+        //If allAttendant true is because all users are presents so there is no reason to pause anyone
 
-    bla = function(){
-    return new Promise(function(fulfill, reject){
-        count_downs = bot.getScore();
-        fulfill(resultValue);
-    });
-};
+        if(allAttendant)
+        {
+            bot.sendChat("`Todos estão presentes.`");
+        }
+        else
+        {
+            for(let i = 0; i < queue.length; i++)
+            {
+                if(attendingList.indexOf(queue[i]) == -1)
+                {
+                    bot.moderatePauseDJ(queueUser[i], function() {});
+                    // LOGS
+                    // console.log(queueUser[i]);
+                    bot.sendChat("Retirando " + queue[i] + " da fila.");
+                }
+            }
+        }
+
+        queue = [];
+        queueUser = [];
+        attendingList = [];
+    }
+
+    async function startAttendanting(time)
+    {
+        attendanting = 1;
+        // LOGS
+        // console.log("attendanting set to 1");
+        bot.sendChat("@djs CHAMADA 1/5");
+        for(let i = 2; i <= 5; i++)
+        {
+            if(attendanting == 0)
+                break;
+            await sleep(250*time);
+            bot.sendChat("@djs CHAMADA " + i + "/5");
+
+        }
+        if(attendanting == 1)
+            finishAttendanting(false);
+    }
+
+    function sleep(ms)
+    {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    function isMod(user)
+    {
+        return (bot.isMod(user) || bot.isManager(user) || bot.isOwner(user) ||  bot.isCreator(user))
+    }
+
+    if (err)
+    {
+        return console.error(err);
+    }
+
+    let attendanting = 0;
+    let response = 0;
+    let queue = [];
+    let queueUser = [];
+    let attendingList = [];
+    let retirados = 0;
+    let botName = "";
+    let myID = "587a90e5a57dd766009509f9";
+    let phrases =
+    [
+        "O sentido da vida é leitar e ser leitado.",
+        ":3",
+        "Credo, um humano falante.",
+        "Que vontade de comer uma trap massa, bixo.",
+        "O Oleg apanha pra mendigo.",
+        "AnaoQ é viado e dá a bunda.",
+        "Toca um rica ai."
+    ];
+    let questions =
+    [
+        "Sim, mas é claro.",
+        "Se você estiver motivado, é possível!",
+        "Sim, com dificuldade",
+        "Provavelmente não.",
+        "Não.",
+        "Nunca.",
+        "Sei lá, cara, te vira, filho da puta.",
+        "Pergunta pra sua mãe, aquela puta.",
+        "Apenas se você estiver tão motivado quanto o mendigo que bateu no Oleg"
+    ];
 
     console.log('Running DubAPI v' + bot.version);
 
-    function connect() {bot.connect('radioanao');}
-
-    function skip ()
+    function connect()
     {
-        count_downs = bot.getScore();
+        bot.connect('radioanao');
     }
 
-    skip = function(){
-
-    return new Promise(function(fulfill, reject){
-        count_downs = bot.getScore();
-        fulfill(count_downs);
-    });
-};
-
-
-
-    function skip2()
+    bot.on('connected', function(name)
     {
-        count_downs = count_downs.downdubs;
-            console.log(count_downs);
-    }
-
-
-    bot.on('connected', function(name) {
         console.log('Connected to ' + name);
-        console.log("before");
-        // console.log(bot.getUserByName("Anao_Engenheiro"));
-        // resp = bot.getQueue();
-        // console.log(bot.getRoomMeta());
-        // console.log(resp);
-        // bot.sendChat("Vou agora retirar o Nyan");
-        //https://api.dubtrack.fm/room/57fd4c0f593d672e002a19e8/queue/user/587a90e5a57dd766009509f9/pause
-        // bot.moderatePauseDJ(bot.getUserByName("Anao_Engenheiro").id, function() {});
-        resp = bot.getQueue();
-        console.log(resp);
-        console.log("after");
-        // while(1)
-        // {
-        //     count_downs = bot.getScore();
-        //     while(count_downs == null);
-        //     count_downs = count_downs.downdubs;
-        //     console.log(count_downs);
-        //     //myPromise.then(console.log(count_downs));
-        //     //count_downs// = resp.downdubs;
-        //     //console.log("count_downs");
-        //     if(count_downs !== 0 && count_downs != null)
-        //     {
-        //         resp = bot.getQueue();
-        //         count_fila = resp.length;
-        //         console.log(count_downs + " " + count_fila);
-        //         if(count_fila <= 2 && count_downs >=3)
-        //             bot.moderateSkip();
-        //         else if (count_downs >= count_fila/2)
-        //             bot.moderateSkip();
-        //     }
-        //     //wait(5000);
-        // }
-
-
+        // LOGS
+        // console.log("before");
+        // response = bot.getQueue();
+        // console.log(response);
+        // console.log("after");
+        bot.sendChat("Chegay :3");
+        botName = bot.getSelf().username;
+        phrases.push("@" + botName + " de onde você veio?");
     });
 
-    function wait(ms){
-   var start = new Date().getTime();
-   var end = start;
-   while(end < start + ms) {
-     end = new Date().getTime();
-  }
-}
-
-    bot.on('disconnected', function(name) {
+    bot.on('disconnected', function(name)
+    {
         console.log('Disconnected from ' + name);
-
         setTimeout(connect, 15000);
     });
 
-    bot.on('error', function(err) {
+    bot.on('error', function(err)
+    {
         console.error(err);
     });
 
     bot.on(bot.events.userJoin, function(name)
     {
-        console.log(name);
-        //bot.sendChat(resp.length + " pessoas na fila :3");
+        // LOGS
+        // console.log(name);
+        bot.sendChat("Bem vindo, " + name.username);
+        bot.sendChat(":3");
     });
 
     bot.on(bot.events.roomPlaylistDub, function(data)
     {
         if(data.type == "room_playlist-dub" && data.dubtype == "downdub")
         {
-            msg = data;
-            count_downs = msg.raw.playlist.downdubs;
-            //console.log(data.playlists);
-            //bot.sendChat("Trigger por downvote");
-            //song = new PlayModel(msg.playlist);
-            resp = bot.getQueue();
-            //count_downs = data.playlist;
-            count_fila = resp.length;
-            //console.log(count_downs + " " + count_fila);
-            if(count_fila <= 2 && count_downs >=3)
+            response = data;
+            let downCount = response.raw.playlist.downdubs;
+            // LOGS
+            // console.log(downCount);
+            response = bot.getQueue();
+            let queueCount = response.length;
+
+            if(queueCount <= 2 && downCount >=3)
             {
                 bot.moderateSkip();
-                bot.sendChat( count_fila + " na fila, " + count_downs + " downvotes.");
+                bot.sendChat( queueCount + " na fila, " + downCount + " downvotes.");
             }
-            else if ((count_downs >= count_fila/2) && count_fila > 2)
+            else if ((downCount >= queueCount/2) && queueCount > 2)
             {
                 bot.moderateSkip();
-                bot.sendChat( count_fila + " na fila, " + count_downs + " downvotes.");
+                bot.sendChat( queueCount + " na fila, " + downCount + " downvotes.");
             }
         }
-
     });
 
-
-    bot.on(bot.events.chatMessage, function(data) {
-        if((data.message.indexOf("help") !== -1) && (data.user.username.indexOf("AnaoTestBot") == -1) && (data.message.indexOf("@AnaoTestBot") !== -1) && (bot.isMod(data.user) || bot.isManager(data.user) || bot.isOwner(data.user) ||  bot.isCreator(data.user)))
+    bot.on(bot.events.chatMessage, function(data)
+    {
+        // LOGS
+        // console.log("attendanting: " + attendanting);
+        if((data.message.indexOf("@"+botName) !== -1) && (isMod(data.user) || (data.user.id === myID)))
         {
-             bot.sendChat("Para a `Staff` realizar chamada, basta me quotar com a palavra `chamada` e após a contagem, me quotar com a palavra `finalizar`.");
-        }
-        // else if((data.message.indexOf("help") !== -1) && (data.user.username.indexOf("AnaoTestBot") == -1) && (data.message.indexOf("@AnaoTestBot") !== -1))
-        // {
-        //     bot.sendChat("Caso a música tenha os `downvotes necessários`, basta digitar algo no chat para que eu `pule` a música.")
-        // }
-        else if(data.message.indexOf("@AnaoTestBot") !== -1)
-        {
-            if(bot.isMod(data.user) || bot.isManager(data.user) || bot.isOwner(data.user) ||  bot.isCreator(data.user) || (data.user.username.indexOf("Anao_Engenheiro") !== -1))
-            {
-               //bot.sendChat("@" + data.user.username + " é da Staff.");
-               if(data.message.indexOf("chamada") !== -1 && chamada == 0)
-               {
-                    if(presentes.indexOf(data.user.username) == -1)
-                    {
-                        presentes.push(data.user.username);
-                    }
-                    resp = bot.getQueue();
-                    console.log(resp);
-                    //resp = JSON.parse(resp);
-                    for(var i = 0; i < resp.length; i++)
-                    {
-                        //console.log(resp[i].user.username);
-                        //bot.sendChat(resp[i].user.username);
-                        fila.push(resp[i].user.username);
-                        fila_user.push(resp[i].uid);
-                        console.log("fila[" + i + "] = " + fila[i]);
-                    }
-
-                    chamada = 1;
-
-                    for(var i =0; i < 5; i++)
-                        bot.sendChat("@djs CHAMADA " + (i+1) + "/5");// CHAMADA RESPONDAM APENAS UMA VEZ " + (i+1) + "/5");
-               }
-               else if((data.message.indexOf("finalizar") !== -1) && (chamada == 1))
-               {
-                    if(presentes.indexOf(data.user.username) == -1)
-                    {
-                        presentes.push(data.user.username);
-                    }
-                //count_downs = bot.getScore();
-                    chamada = 0;
-                    for(var i = 0; i < fila.length; i++)
-                        console.log("fila: " + fila[i]);
-                    for(var i = 0; i < presentes.length; i++)
-                        console.log("presentes: " + presentes[i]);
-                    //{
-                    for(var j = 0; j < fila.length; j++)
-                        if(presentes.indexOf(fila[j]) == -1)
-                        {
-                            bot.moderatePauseDJ(fila_user[j], function() {});
-                            //bot.moderateRemoveSong(fila_user[j]);
-                            console.log(fila_user[j]);
-                            bot.sendChat("Retirando @" + fila[j] + " da fila.");
-                            retirados ++;
-                        }
-                    if(retirados == 0)
-                        bot.sendChat("`Todos estão presentes.`");
-                    else
-                        retirados = 0;
-                    fila = [];
-                    fila_user = [];
-                    presentes = [];
-                        //console.log("fila: " + fila[i]);
-                    //}
-
-                        //console.log("presentes: " + presentes[i]);
-                }
-                else if ((data.user.username.indexOf("Anao_Engenheiro") !== -1) && (data.message.indexOf("skip") !== -1))
-                    bot.moderateSkip();
-                else if (data.user.username.indexOf("Anao_Engenheiro") !== -1)
+           if(data.message.indexOf("chamada") !== -1 && attendanting === 0)
+           {
+                if(attendingList.indexOf(data.user.username) === -1)
                 {
-                    var testi = false;
-                    if(testi)
-                            console.log("fez coisa");
-                    console.log("entrou");
-                    bot.sendChat("Testando timer: 0", function () {
-                        testi = true;
-                        wait(1000*30);
-                        console.log("acabou espera");
-                        bot.sendChat("Apos 30 segundos");
-                    });
-                    // console.log("acabou espera");
-                    // bot.sendChat("Apos 30 segundos");
-
-                    // bot.moderatePauseDJ(bot.getUserByName("Anao_Engenheiro").id, function() {});
-                    // var resp = bot.getQueue();
-                    // console.log(resp);
+                    attendingList.push(data.user.username);
                 }
-
-               // else
-               //  bot.sendChat(":thumbsup: (me quote com `help` para mais informações) ");
-            // else if(data.message.indexOf("qual o sentido da vida?") !== -1)
-            //     bot.sendChat("Leitar e ser leitado.");
-            // else if(data.message.indexOf("ping") !== -1)
-            //     bot.sendChat("@" + data.user.username + " pong :3");
-            // //else if (data.user.username.indexOf("Poneiamigo") !== -1)
-            // //{
-            //     //bot.sendChat("@" + data.user.username + " sua puta.");
-            // //}
-            // else if ((data.message.indexOf("puta") !== -1) || (data.message.indexOf("vagabunda") !== -1))
-            //     bot.sendChat("Me enche de leitinho quente :3 , " + "@" + data.user.username);
-
+                response = bot.getQueue();
+                // LOGS
+                // console.log(response);
+                for(let i = 0; i < response.length; i++)
+                {
+                    queue.push(response[i].user.username);
+                    queueUser.push(response[i].uid);
+                    // LOGS
+                    // console.log("queue[" + i + "] = " + queue[i]);
+                }
+                startAttendanting(60);
             }
-
-            // else
-            //     bot.sendChat(":thumbsup: (me quote com `help` para mais informações) ");
+            else if (data.user.id === myID)
+            {
+                if(data.message.indexOf("skip") !== -1)
+                {
+                    bot.moderateSkip();
+                }
+                else if(data.message.indexOf("?") !== -1)
+                {
+                    bot.sendChat(questions[Math.floor(Math.random()*questions.length)]);
+                }
+                else
+                {
+                    bot.sendChat(phrases[Math.floor(Math.random()*phrases.length)]);
+                }
+            }
         }
-            else if(chamada == 1)
-         {
-             //console.log(data.user.username + ': ' + data.message);
-            //  for(var i = 0; i < resp.length; i++)
-            // {
-            //     console.log( "fila[" + i + "] = " + fila[i] + " " + data.user.username);
-            //      if(fila[i] == data.user.username)
-            //      {
-            //          //bot.sendChat("@" + data.user.username + " presente");
-            //          console.log( "fila[" + i + "] = " + fila[i] + " presente");
-            //          fila[i] = 0;
-            //          //console.log( fila[fila.indexOf(data.user.username)] + " presente");
-            //          console.log( "fila[" + i + "] = " + fila[i] + " zerado");
-            //          break;
-            //      }
-            // }
-            if(presentes.indexOf(data.user.username) == -1)
-            {
-                presentes.push(data.user.username);
-            }
-         }
-
-
-        /*count_downs = bot.getScore();
-        count_downs = count_downs.downdubs;
-        if(count_downs !== 0 && count_downs != null && ((data.user.username.indexOf("AnaoTestBot") == -1)))
+        else if(attendanting == 1)
         {
-            resp = bot.getQueue();
-            count_fila = resp.length;
-            //console.log(count_downs + " " + count_fila);
-            if(count_fila <= 2 && count_downs >=3)
+            if((attendingList.indexOf(data.user.username) === -1) && (queue.indexOf(data.user.username) !== -1))
             {
-                bot.moderateSkip();
-                bot.sendChat( count_fila + " na fila, " + count_downs + " downvotes.");
+                attendingList.push(data.user.username);
+                bot.sendChat("`" + data.user.username + " presente " + attendingList.length + "/" + queue.length + "`");
+                if(attendingList.length === queue.length)
+                {
+                    finishAttendanting(true);
+                    attendanting = 0;
+                    bot.sendChat("`Todos estão presentes.`");
+                }
             }
-            else if ((count_downs >= count_fila/2) && count_fila > 2)
-            {
-                bot.moderateSkip();
-                bot.sendChat( count_fila + " na fila, " + count_downs + " downvotes.");
-            }
-        }*/
-
-        //console.log(data.user.username + ': ' + data.message);
+        }
     });
 
     connect();
-
-
-
-    // while(1)
-    // {
-    //     resp = bot.getScore();
-    //     console.log(resp);
-    //     //myPromise.then(console.log(count_downs));
-    //     count_downs = resp.downdubs;
-    //     console.log(count_downs);
-    //     if(count_downs !== 0)
-    //     {
-    //         resp = bot.getQueue();
-    //         count_fila = resp.length;
-    //         //console.log(count_downs + " " + count_fila);
-    //         if(count_fila <= 2 && count_downs >=3)
-    //             bot.moderateSkip();
-    //         else if (count_downs >= count_fila/2)
-    //             bot.moderateSkip();
-    //     }
-    // }
-
 });
